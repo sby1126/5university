ad367ac220d78cd9f6f6b0ad55f479a3cacf9e2b
NewStar <yimsb1126@naver.com>
2024-08-24 20:48:46 +0900

    [fix] settings

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    assetPrefix:
+        "https://sby1126.github.io/5university/"
+    ,
+};
 
 export default nextConfig;
