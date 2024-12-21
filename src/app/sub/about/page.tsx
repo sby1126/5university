d031a2f058cc9a84e69ac2bdd372bd16b7128aa6
NewStar <yimsb1126@naver.com>
2024-12-22 00:01:27 +0900

    about-mobile

--- src/app/sub/about/page.tsx
+++ src/app/sub/about/page.tsx
@@ -53,7 +53,7 @@ export default function ArtsPage() {
   }, []);
   return (
     <div
-      className="w-full h-full flex justify-center items-center pt-10"
+      className="w-full h-full flex justify-center items-center pt-20 md:pt-10"
     >
       <div className="text-gray-900 leading-normal tracking-wider bg-cover">
         <div className="max-w-4xl flex items-center h-auto lg:h-full flex-wrap">
