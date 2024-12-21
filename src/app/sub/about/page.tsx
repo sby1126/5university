456fdb765968c29bdbbdf3a9f2c3a2e86b76789b
NewStar <yimsb1126@naver.com>
2024-12-22 00:27:36 +0900

    about

--- src/app/sub/about/page.tsx
+++ src/app/sub/about/page.tsx
@@ -53,7 +53,7 @@ export default function ArtsPage() {
   }, []);
   return (
     <div
-      className="w-full h-full flex justify-center items-center pt-20 md:pt-10"
+      className="w-full h-full flex justify-center items-center pt-28 md:pt-10"
     >
       <div className="text-gray-900 leading-normal tracking-wider bg-cover">
         <div className="max-w-4xl flex items-center h-auto lg:h-full flex-wrap">
@@ -62,10 +62,9 @@ export default function ArtsPage() {
             id="profile"
             className="w-full lg:w-3/5 rounded-lg lg:rounded-l-lg lg:rounded-r-none shadow-2xl bg-white opacity-95 mx-6 lg:mx-0"
           >
-            <div className="p-4 md:p-12 text-center lg:text-left">
+            <div className="p-12 text-center lg:text-left">
               {/* <!-- Image for mobile view--> */}
               <div className="block lg:hidden rounded-full shadow-xl mx-auto -mt-16 h-48 w-48 bg-cover bg-center bg-[url('/images/5university_Profile.jpeg')]" />
-
               <h1 className="text-3xl font-bold pt-8 lg:pt-0">
                 {myProfile?.name}
               </h1>
